599931cbf0e06733c1f2e465356401669347cc4a
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2023-09-18 13:35:20 -0600

    Add project files.

---
+++ Code quest code/Code_quest_code.py
@@ -0,0 +1,19 @@
+
+number = float(input("Enter a number: "))
+
+rounded_number = round(number)
+
+def num():
+
+    if number > 0:
+        print("POSITIVE")
+
+    else:
+        print("NEGATIVE")
+
+
+for j in range(num):
+
+
+
+
